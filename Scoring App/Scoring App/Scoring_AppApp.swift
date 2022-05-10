f0d1e907dcdcb6e13466de76d1761b04e83a3d11
abdWasiq <abdul.wasiq@naheed.pk>
2022-05-10 15:57:58 +0500

    project created

---
+++ Scoring App/Scoring App/Scoring_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scoring_AppApp.swift
+//  Scoring App
+//
+//  Created by NaheedPK on 10/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Scoring_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
